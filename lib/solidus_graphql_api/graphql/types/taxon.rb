38290baa3a12e449bdd64fe473aab88a9825ade2
Davor Ocelic <docelic@crystallabs.io>
2018-10-16 17:48:10 +0200

    Resolve merge conflicts

--- lib/solidus_graphql_api/graphql/types/taxon.rb
+++ lib/solidus_graphql_api/graphql/types/taxon.rb
@@ -1,18 +1,20 @@
 module Spree
   module GraphQL
     module Types
-      TaxonType = ::GraphQL::ObjectType.define do
+      class Taxon < BaseObject
         graphql_name 'Taxon'
 
         field :id, ID, null: true
-        field :graphql_name, String, null: true
+        field :name, String, null: true
+        field :position, Integer, null: true
+        field :description, String, null: true
+        field :icon_file_name, String, null: true
+
         field :permalink, String, null: true
         field :pretty_graphql_name, String, null: true
         field :seo_title, String, null: true
 
-        field :products, Product.connection_type, null: true, connection: true do
-          resolve Resolvers::Product::ByTaxon
-        end
+        field :products, [Product], null: true, resolve: Resolvers::Product::ByTaxon
       end
     end
   end
