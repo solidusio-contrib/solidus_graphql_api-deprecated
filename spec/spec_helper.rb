f58389ec411fd682ff34eaf0baa7c7dc4ea892d7
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-03-26 12:48:05 +0100

    Refactor RSpec configuration
    It includes the following changes:
    
    - use `solidus_support` gem in order to import standard RSpec
      configuration for Solidus extensions
    - remove redundant customizations
    - assign `:graphql` type to specs living within `spec/graphql`
    - restrict `Spree::GraphQL::Spec::Helpers` to `:graphql` type specs
    - fix wrong action calls within controller specs: GitHub user
      @EduardoGHdez in [another PR's
      discussion](https://github.com/boomerdigital/solidus_graphql_api/pull/36#pullrequestreview-219023599)
      made me smell a RSpec misconfiguration, where `config.include
      RSpec::Rails::RequestExampleGroup` was overwriting controller type
      methods with request/integration type methods, leading to confusion
      and not appropriate controller specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,47 +6,29 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'factory_bot'
 require 'spree/testing_support/factories'
-require "spree/testing_support/preferences"
-#require 'spree/testing_support/authorization_helpers'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-require "database_cleaner"
+require 'spree/testing_support/preferences'
+require 'solidus_support/extension/rails_helper'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.backtrace_exclusion_patterns = [/gems\/activesupport/, /gems\/actionpack/, /gems\/rspec/]
-  config.color = true
   config.infer_spec_type_from_file_location!
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-  config.mock_with :rspec do |c|
-    c.syntax = :expect
+
+  config.define_derived_metadata(file_path: Regexp.new('/spec/graphql/')) do |metadata|
+    metadata[:type] = :graphql
   end
 
-  config.before(:each) do
-    Rails.cache.clear
-    reset_spree_preferences
-  end
-
-  config.use_transactional_fixtures = true
-  config.example_status_persistence_file_path = "./spec/samples.txt"
-  config.order = :random
-  Kernel.srand config.seed
-
-  config.include FactoryBot::Syntax::Methods
-  config.include Spree::TestingSupport::Preferences
-  config.include RSpec::Rails::RequestExampleGroup
-  config.include Spree::GraphQL::Spec::Helpers
+  config.include Spree::GraphQL::Spec::Helpers, type: :graphql
 
   config.before do
     Spree::Core::Engine.routes.draw do
-      get "/graphql", to: "graphql#execute"
-      post "/graphql", to: "graphql#execute"
+      get :graphql, to: 'graphql#execute'
+      post :graphql, to: 'graphql#execute'
     end
   end
   config.after(:all) do
-    # Spree::Core::Engine.routes.reload_routes!
     Rails.application.reload_routes!
   end
 end
