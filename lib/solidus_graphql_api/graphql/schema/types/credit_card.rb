0edc0fa96c9644f7718f5e8ce75b04fc9ebdb5b3
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-04-05 10:19:13 +0200

    Merge CreditCard implementation

--- lib/solidus_graphql_api/graphql/schema/types/credit_card.rb
+++ lib/solidus_graphql_api/graphql/schema/types/credit_card.rb
@@ -1,29 +1,48 @@
 class Spree::GraphQL::Schema::Types::CreditCard < Spree::GraphQL::Schema::Types::BaseObject
   graphql_name 'CreditCard'
+
   description %q{Credit card information used for a payment.}
-  include ::Spree::GraphQL::Types::CreditCard
-  field :brand, ::GraphQL::Types::String, null: true do
-    description nil
+
+  field :brand, ::GraphQL::Types::String, null: true
+  def brand
+    object.brand.upcase
   end
-  field :expiry_month, ::GraphQL::Types::Int, null: true do
-    description nil
+
+  field :expiry_month, ::GraphQL::Types::Int, null: true
+  def expiry_month
+    object.month
   end
-  field :expiry_year, ::GraphQL::Types::Int, null: true do
-    description nil
+
+  field :expiry_year, ::GraphQL::Types::Int, null: true
+  def expiry_year
+    object.year
   end
-  field :first_digits, ::GraphQL::Types::String, null: true do
-    description nil
+
+  field :first_digits, ::GraphQL::Types::String, null: true
+  def first_digits
+    raise ::Spree::GraphQL::NotImplementedError.new
   end
-  field :first_name, ::GraphQL::Types::String, null: true do
-    description nil
+
+
+  field :first_name, ::GraphQL::Types::String, null: true
+  def first_name
+    object.first_name
   end
-  field :last_digits, ::GraphQL::Types::String, null: true do
-    description nil
+
+  field :last_digits, ::GraphQL::Types::String, null: true
+  def last_digits
+    object.last_digits
   end
-  field :last_name, ::GraphQL::Types::String, null: true do
-    description nil
+
+  field :last_name, ::GraphQL::Types::String, null: true
+  def last_name
+    object.last_name
   end
+
   field :masked_number, ::GraphQL::Types::String, null: true do
     description %q{Masked credit card number with only the last 4 digits displayed}
+  end
+  def masked_number
+    object.display_number
   end
 end
