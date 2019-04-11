ca4211ce0c92d57257388e8097fe35ad8a303f6b
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-04-11 11:49:28 +0200

    Remove lib/spree files glob require
    Now that Domain has been moved under `lib/solidus_graphql_api/graphql`,
    `lib/spree` is empty, so we can remove its configuration. In the future,
    we'll probably evaluate whether `lib/solidus_graphql_api` is a right
    name for files living within `Spree` namespace.

--- lib/solidus_graphql_api/engine.rb
+++ lib/solidus_graphql_api/engine.rb
@@ -16,9 +16,6 @@ module Spree
         Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
-        Dir.glob(File.join(File.dirname(__FILE__), '../../lib/spree/**/*.rb')) do |c|
-          Rails.configuration.cache_classes ? require(c) : load(c)
-        end
       end
 
       config.to_prepare(&method(:activate).to_proc)
