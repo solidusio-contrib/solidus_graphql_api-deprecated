551b2b6a557848aee903154864f26d3a20c8188e
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-04-12 10:47:16 +0200

    Handle code loading with Zeitwerk
    [Zeitwerk](https://github.com/fxn/zeitwerk) is a new code loader for
    Ruby, which will be the default code loader for Rails 6. Its integration
    allows us the removal of empty namespaces definitions, internal code
    requires and circular dependencies conflicts hacks, such as the
    definition of another constant used within the costant of the current
    file.

--- lib/solidus_graphql_api.rb
+++ lib/solidus_graphql_api.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'graphql'
 require 'solidus_core'
+require 'zeitwerk'
 
 module SolidusGraphqlApi; end
 
-require 'solidus_graphql_api/engine'
-require 'spree/graphql'
+require 'solidus_graphql_api/zeitwerk_inflector'
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector = SolidusGraphqlApi::ZeitwerkInflector.new __FILE__
+loader.ignore "#{__dir__}/solidus_graphql_api/zeitwerk_inflector.rb"
+loader.setup
+loader.eager_load
--- lib/spree/graphql.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Spree::GraphQL; end
-
-require_relative './graphql/schema'
