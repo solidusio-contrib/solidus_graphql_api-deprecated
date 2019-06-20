512f18371aeb6f47cba4a65be4772db8fe1aa11b
jacobherrington <jacobherringtondeveloper@gmail.com>
2019-06-19 21:24:39 -0500

    Change decorator file structure
    This is not an urgent change, but for the sake of establishing this
    pattern before more decorators are added, I think it should be done now.
    
    I prefer this way of organizing decorators because it makes it more
    obvious to the developer which classes are intended to be decorators and
    which are completely new models or controllers.

--- lib/solidus_graphql_api/engine.rb
+++ lib/solidus_graphql_api/engine.rb
@@ -17,8 +17,8 @@ class SolidusGraphqlApi::Engine < Rails::Engine
   end
 
   def self.activate
-    Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-      Rails.configuration.cache_classes ? require(c) : load(c)
+    Dir.glob(File.join(File.dirname(__FILE__), '../../app/decorators/**/*.rb')) do |c|
+      require_dependency(c)
     end
   end
 
